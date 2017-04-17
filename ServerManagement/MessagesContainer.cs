066e4bddd1ef876b5b3c938c897981e282a76852
rachid1805 <raannn@hotmail.com>
2017-04-17 19:39:44 -0400

    Code cleaup

--- ServerManagement/MessagesContainer.cs
+++ ServerManagement/MessagesContainer.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace ServerManagement
 {
-  class MessagesContainer : IMessagesManager
+  class MessagesContainer
   {
     private readonly List<RoomArchiveContainer> _roomsArchive;
 
